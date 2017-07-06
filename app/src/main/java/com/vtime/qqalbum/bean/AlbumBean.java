180f69c66215f088a1fbb52b6c0d685108edc013
Your Name <2540999800@qq.com>
2017-07-06 16:15:40 +0800

    1

---
+++ app/src/main/java/com/vtime/qqalbum/bean/AlbumBean.java
@@ -0,0 +1,27 @@
+package com.vtime.qqalbum.bean;
+
+/**
+ * Created by Jue on 2017/7/6.
+ */
+
+public class AlbumBean {
+
+    private String title;
+    private String subId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubId() {
+        return subId;
+    }
+
+    public void setSubId(String subId) {
+        this.subId = subId;
+    }
+}
---
+++ app/src/main/java/com/vtime/qqalbum/ui/MainActivity.java
@@ -0,0 +1,23 @@
+package com.vtime.qqalbum.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.vtime.qqalbum.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        initData();
+
+    }
+
+    private void initData() {
+    }
+
+}
